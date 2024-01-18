5ea9ebe8df61d8e219f703f03138353029dcd2f9
Lucas Migliorin da Rosa <lucasmigliorin@hotmail.com>
2024-01-17 20:35:26 -0400

    fix: Change dict to object

--- user/user.py
+++ user/user.py
@@ -1,5 +1,8 @@
 import crypto.elgamal as elgamal
 
+class Struct:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
 
 class User():
     def __init__(self,user_name) -> None:
@@ -9,6 +12,7 @@ class User():
         self.user_name = user_name
 
         self.know_user = {}
+        self.finger_print = None
 
     def add_know_user(self,user_name,public_key):
         if(user_name in list(self.know_user.keys())):
@@ -21,7 +25,10 @@ class User():
         if(user not in list(self.know_user.keys())):
             return False
         else:
-            return elgamal.encrypt(self.know_user[user], msg)
+            aux = self.know_user[user]
+            if isinstance(aux,dict):
+                aux = Struct(**aux)
+            return elgamal.encrypt(aux, msg)
 
     def receive_msg(self,msg_crypto:str)->str:
         return elgamal.decrypt(self.private_key, msg_crypto)
